b3e769b17d5d50aa7e0dab8d7c176cb8a2025101
Tikkumies <mikko_stick@hotmail.com>
2022-10-22 23:20:47 +0300

    Added attack_calculations method

--- FoodBrawl/game/loop.py
+++ FoodBrawl/game/loop.py
@@ -7,6 +7,7 @@ class Loop:
         self.character1 = character1
         self.character2 = character2
         self.screen = screen
+        self.time = 0
 
     def draw_fight_items(self):
         # Value that right hand side healthbar has to move right after hp loss
@@ -18,13 +19,24 @@ class Loop:
         self.draw.blit(self.screen, self.character1.healthbar_name_text, self.screen.width - 170, 100)
         self.draw.blit(self.screen, self.character1.image, 850 , 200)
         self.draw.blit(self.screen, self.character2.image, 50, 400)
+
+    def attack_calculations(self):
+        if self.char1_attack_time == self.time - 1:
+            self.char1_attack_time += self.character1.delay
+        if self.char2_attack_time == self.time - 1:
+            self.char2_attack_time += self.character2.delay
+        # Attack activated
+        if self.char2_attack_time == self.time:
+            self.character1.hp_loss(self.character2, self.character1, self.screen)
+        if self.char1_attack_time == self.time:
+            self.character2.hp_loss(self.character1, self.character2, self.screen)
         
     def loop(self):
         running = True
         self.clock.tick(self.fps)
-        char1_attack_time = self.character1.delay
-        char2_attack_time = self.character2.delay
-        time = 0
+        self.char1_attack_time = self.character1.delay
+        self.char2_attack_time = self.character2.delay
+
         while running:
             for event in self.pygame.event.get():
                 if event.type == self.pygame.QUIT:   
@@ -32,8 +44,8 @@ class Loop:
 
             match self.screen.game_screen_selection:
                 case "menu":
-                    time += 1
-                    if time == 1:
+                    self.time += 1
+                    if self.time == 1:
                         # Play menu music
                         self.screen.music_menu.play(-1)
                     self.draw.blit(self.screen, self.screen.background, 0, 0)
@@ -45,33 +57,25 @@ class Loop:
                     if self.pygame.mouse.get_pressed()[0] and self.screen.start_button.collidepoint(self.pygame.mouse.get_pos()):
                         self.screen.game_screen_selection = "game"
                         self.screen.music_menu.stop()
-                        time = 0
+                        self.time = 0
 
                 case "game":
                     # Play fight music
-                    if time == 1:
+                    if self.time == 1:
                         self.screen.music_fight.play(-1)
                     # Attack delay calculation
-                    if char1_attack_time == time - 1:
-                        char1_attack_time += self.character1.delay
-                    if char2_attack_time == time - 1:
-                        char2_attack_time += self.character2.delay
-                    # Attack activated
-                    if char2_attack_time == time:
-                        self.character1.hp_loss(self.character2, self.character1, self.screen)
-                    if char1_attack_time == time:
-                        self.character2.hp_loss(self.character1, self.character2, self.screen)
+                    self.attack_calculations()
                     # Draw fight related stuff
                     self.draw_fight_items()
                     # Attack animations
-                    if time == char2_attack_time:
+                    if self.time == self.char2_attack_time:
                         self.draw.blit(self.screen, self.character2.image_fire, 390 , 530)
-                    if time == char1_attack_time:
+                    if self.time == self.char1_attack_time:
                         self.draw.blit(self.screen, self.character1.image_fire, 290 , 470)
                     # Check if characters health is zero or less 
                     if self.character1.hp < 1 or self.character2.hp < 1:
                         self.screen.game_screen_selection = "game over"
-                    time += 1
+                    self.time += 1
 
                 case "game over":
                     self.draw_fight_items()
@@ -88,17 +92,17 @@ class Loop:
                         self.character2.reset_character()
                         self.screen.game_screen_selection = "menu"
                         self.screen.music_fight.stop()
-                        time = 0
+                        self.time = 0
                     # Start new game
                     if self.pygame.mouse.get_pressed()[0] and self.screen.new_game_button.collidepoint(self.pygame.mouse.get_pos()):
                         self.character1.reset_character()
                         self.character2.reset_character()
                         self.screen.game_screen_selection = "game"
                         self.screen.music_fight.stop()
-                        time = 0
+                        self.time = 0
 
-                    char1_attack_time = 0
-                    char2_attack_time = 0
+                    self.char1_attack_time = 0
+                    self.char2_attack_time = 0
 
             self.pygame.display.update()
             self.pygame.time.wait(100)
